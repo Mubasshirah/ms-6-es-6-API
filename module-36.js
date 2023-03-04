853bb84296d98f000b86baf961729ed9bbdf6742
Mubasshirah <adibaju1994@gmail.com>
2023-03-04 09:53:32 +0600

    d

---
+++ module-36.js
@@ -0,0 +1,80 @@
+// const player={};
+// player.name='sakib';
+// player.gender='male';
+// player.bat=function(){
+//     console.log('spin  the bat');
+
+// }
+// console.log(player);
+// player.bat()
+// const atel=Object.create(null);
+// console.log(atel)
+// const batel=Object.create(player);
+// console.log(batel)
+// console.log(batel.name)
+
+// class person{
+//     name='aiba';
+//     address='mirpur';
+//     constructor(age){
+//         this.age=age;
+//     }
+// }
+// const person1=new person(30);
+// console.log(person1)
+//  const student={
+//     name:'adiba',
+//     address:'mirpur',
+//     money:5000,
+//     subject:['math','physics','chemistry'],
+//     exam:function(){
+//         console.log(this.name, 'your examat 9pm')
+//     },
+//     treatday(treatMoney,tips){
+//         this.money=this.money-treatMoney-tips;
+//         return `${treatMoney} tk khauanor por or kache ache ${this.money}`;
+//     }
+//  }
+//  student.exam();
+//  const firstDola=student.treatday(1000,100);
+//  console.log(firstDola);
+// const object={name:'adiba',age:'30',job:'unemployed'}
+// console.log(Object.keys(object) )
+// console.log(Object.values(object) )
+// console.log(Object.entries(object))
+const object={name:'adiba',age:'30',job:'unemployed'};
+// const keys=Object.keys(object);
+// console.log(keys);
+// for(const key of keys){
+// const allEntries=object[key];
+// console.log(key,allEntries)
+// }
+// for(const key in object){
+//     console.log(key,object[key]);
+// }
+
+// const first=2;
+// const second=2;
+// if(first===second){
+//     console.log('they are same')
+// }
+// else{
+//     console.log('they are different');
+// }
+// const first1={a:2};
+// const second1={a:2};
+// if(first1===second1){
+//     console.log('they are same')
+// }
+// else{
+//     console.log('they are different');
+// }
+const first1={a:2};
+const second1={a:2};
+const third1=second1;
+if(third1===second1){
+    console.log('they are same')
+}
+else{
+    console.log('they are different');
+}
---
+++ module-37.js
@@ -0,0 +1,87 @@
+// let firstObj={name:'ali',age:20};
+// let secondObj=firstObj;
+// console.log(firstObj,secondObj);
+// // secondObj={name:'kali',age:30};
+// console.log(firstObj,secondObj)
+// secondObj.age=400;
+// console.log(firstObj,secondObj);
+
+// function count(){
+//     let counting=0;
+//     return function(){
+//         counting++;
+//         return counting;
+//     }
+// }
+
+// const count1=count();
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())
+// const count2=count();
+// console.log(count2());
+// console.log(count2());
+// console.log(count2());
+// console.log(count2());
+// console.log(count2());
+
+
+// function sum(a,b,c){
+//     console.log(arguments);
+//     const result=a+b+c;
+//     return result;
+// }
+// const sum2=sum(1,2,3);
+// console.log(sum2);
+// const sum1=sum(1,2,3,4,5,6);
+// console.log(sum1);
+
+// const num11=5;
+// const num22=7;
+// function multiply(num1,num2){
+//     num1=10;
+//     const result=num1*num2;
+//     return result;
+// }
+// console.log(multiply(num11,num22));
+// console.log(num11,num22)
+
+// const stu1={name:'adiba',age:30};
+// const stu2={name:'tanjib',age:40};
+// function couple(first,second){
+//  first.name="mubasshirah";
+//  second.age=100;
+// }
+// console.log(stu1,stu2);
+// couple(stu1,stu2);
+// console.log(stu1,stu2)
+
+// const para='kamil';
+// console.log(isNaN(para))
+
+// let falsyValue=undefined;
+// if(falsyValue){
+//     console.log('value is true')
+// }
+// else{
+//     console.log('value is false')
+// }
+
+const operation=(a,b,c)=>{
+    if(c==='add'){
+        return a+b;
+    
+    }
+    else if(c==='subtract'){
+        return a-b;
+    }
+    else if(c==='devide'){
+        return a/b
+    }
+    else{
+        return 'invalid';
+    }
+}
+console.log(operation(2,3,'devide'));
